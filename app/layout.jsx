c6e08ba620197f6c05c84bedd455945b2eaa3299
Conner-Berg <connerwberg@gmail.com>
2023-05-13 11:26:43 -0400

    Add Nav comp for dev mode testing

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,8 @@
 import "@styles/globals.css";
 
-export const metadta = {
+import Nav from "@components/Nav";
+
+export const metadata = {
 	title: "Promptopia",
 	description: "Discover & Share AI Prompts",
 };
@@ -13,7 +15,10 @@ const RootLayout = ({ children }) => {
 					<div className="gradient" />
 				</div>
 
-				<main className="app">{children}</main>
+				<main className="app">
+					<Nav />
+					{children}
+				</main>
 			</body>
 		</html>
 	);
