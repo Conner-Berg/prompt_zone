45144de8cbba4f7337811bfd5e843a4d25029bab
Conner-Berg <connerwberg@gmail.com>
2023-05-14 15:11:46 -0400

    Create handler for provider to database logic

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,58 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+import User from "@models/User";
+import { connectToDB } from "@utils/database";
+
+// Every Next.js route is a serverless route
+// serverless -> lambda
+// Only opens up when it's called
+// Every time it gets called, it spins up a server, and makes a connection to the database
+// This is so that a server doesn't have to constantly be running
+
+console.log({
+	clientId: process.env.GOOGLE_ID,
+	clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+});
+
+const handler = NextAuth({
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+		}),
+	],
+	async session({ sesson }) {
+		const sessionUser = await User.findOne({ email: session.user.email });
+
+		sesson.user.id = sessonUser._id.toString();
+
+		return session;
+	},
+	async signIn({ profile }) {
+		try {
+			await connectToDB();
+
+			// Check if user exists
+			const userExists = await User.findOne({
+				email: profile.email,
+			});
+
+			// If not, create user and save to database
+			if (!userExists) {
+				await User.create({
+					email: profile.email,
+					username: profile.name.replace(" ", "").toLowerCase(),
+					image: profile.picture,
+				});
+			}
+
+			return true;
+		} catch (error) {
+			console.log(error);
+			return false;
+		}
+	},
+});
+
+export { handler as GET, handler as POST };
